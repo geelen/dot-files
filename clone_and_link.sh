310191e67f7ee8f77a3ffc8779578c162972d3a3
geelen <glen83@gmail.com>
2009-10-15 03:10:00 -0700

    copied ben's latest

--- clone_and_link.sh
+++ clone_and_link.sh
@@ -1,8 +1,10 @@
+#!/bin/bash
+ 
 cd &&
-[ -d '.dot-files' ] || git clone git://github.com/geelen/dot-files.git .dot-files &&
-ls -1d --color=none .dot-files/.* | while read f; do
+[ -d '.dot-files' ] || git clone git://github.com/benhoskings/dot-files.git .dot-files &&
+ls -1d .dot-files/.* | while read f; do
   [ "$f" == '.dot-files/.' ] ||
   [ "$f" == '.dot-files/..' ] ||
   [ "$f" == '.dot-files/.git' ] ||
-  ln -vs "$f" .
+  ln -vsf "$f" .
 done
