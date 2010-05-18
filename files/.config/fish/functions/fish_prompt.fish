5df616e4a28ad0cb24fb774ef72d6775ec27f474
Glen Maddern <glenmaddern@gmail.com>
2010-05-18 11:24:51 +1000

    hostname is now in red by default

--- files/.config/fish/functions/fish_prompt.fish
+++ files/.config/fish/functions/fish_prompt.fish
@@ -3,11 +3,11 @@ function fish_prompt --description 'Write out the prompt'
 	set -l duration $CMD_DURATION
 	set -l pr_timestamp (date '+%a %H:%M:%S')
 	set -l pr_user (whoami)
-	set -l pr_host (hostname | cut -d . -f 1)
+	set -l pr_hostname (hostname | cut -d . -f 1)
+	set -l pr_host (echo "\001\033[31m\002$pr_hostname\001\033[0m\002")
 	set -l pr_duration (test -n "$duration"; and echo " ($duration)")
 	set -l pr_term (test $TERM = 'screen'; and echo "[screen/$WINDOW] ")
 	set -l pr_cwd (prompt_pwd)
 	set -l pr_git_info (git_cwd_info)
 	echo -e "\001\033[90m\002$pr_timestamp$pr_duration\001\033[0m\002 $pr_user\001\033[90m\002@\001\033[0m\002$pr_host \001\033[90m\002$pr_term\001\033[0m\002$pr_cwd\001\033[90m\002$pr_git_info\001\033[0m\002\n$pr_prompt "
-
 end
